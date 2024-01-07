f8445ee6541d900a5293e6b623e9382bed5943ec
Subash <Subashtech14@gmail.com>
2024-01-07 20:11:53 +0530

    Copy Right and about us

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Script for navigation bar
+function news() {
+  let email = document.getElementById("email").value;
+  console.log(email);
+  if (validateEmailAddress(email)) {
+    alert("Thank you for subscribing");
+  } else {
+    alert("Please enter a valid email address");
+  }
+}
+function validateEmailAddress(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
